d392764977ae29ed6e34160fd33c75e87a516666
unknown <nenegrakrina@gmail.com>
2021-11-24 21:18:42 +0300

    idk why it doesn't work. i just wanna to do cart((9(

--- js/partners.js
+++ js/partners.js
@@ -1,12 +1,52 @@
-const renderItems = (data) => {
-  console.log(data);
-}
+const partners = () => {
+  const cardsRestaurants = document.querySelector('.cards-restaurants');
 
-fetch('https://delivery-food-f1dcb-default-rtdb.firebaseio.com/db/partners.json')
-  .then((resp) => resp.json())
-  .then((data) => {
-    renderItems(data);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+  const renderItems = (data) => {
+    data.forEach((item) => {
+      const { image, kitchen, name, price, products, stars, time_of_delivery } = item;
+      const a = document.createElement('a');
+
+      a.setAttribute('href', '/restaurant.html');
+      a.classList.add('card');
+      a.classList.add('card-restaurant');
+
+      a.dataset.products = products;
+
+      a.innerHTML = `
+        <img src="${image}" alt="${name}" class="card-image" />
+        <div class="card-text">
+          <div class="card-heading">
+            <h3 class="card-title">${name}</h3>
+            <span class="card-tag tag">${time_of_delivery} мин</span>
+          </div>
+          <div class="card-info">
+            <div class="rating">${stars}</div>
+            <div class="price">От ${price} ₽</div>
+            <div class="category">${kitchen}</div>
+          </div>
+        </div>
+      `;
+
+      a.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        localStorage.setItem('restaurant', JSON.stringify(item));
+
+        window.location.href = '/restaurant.html';
+      });
+
+      cardsRestaurants.append(a);
+    });
+  };
+
+  fetch('https://delivery-food-f1dcb-default-rtdb.firebaseio.com/db/partners.json')
+    .then((resp) => resp.json())
+    .then((data) => {
+      renderItems(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+partners();
